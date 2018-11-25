9cb3790412528e577a9dc820a30df0a4c669784f
Brady Salz <brady.salz@gmail.com>
2018-11-25 12:36:06 -0800

    [parsers] refactoring to make eventual altera add easier

--- parsers/parse_table.py
+++ parsers/parse_table.py
@@ -1,10 +1,10 @@
-from parsers.parse_xilinx_spartan import parse_xilinx_spartan
-from parsers.parse_xilinx_spartan_six import parse_xilinx_spartan_six
-from parsers.parse_xilinx_virtex_five import parse_xilinx_virtex_five
-from parsers.parse_xilinx_virtex_four import parse_xilinx_virtex_four
-from parsers.parse_xilinx_virtex_six import parse_xilinx_virtex_six
-from parsers.xilinx_ultrascale_plus import parse_kintex_ultrascale_plus
-from parsers.xilinx_ultrascale import parse_kintex_ultrascale
+from parsers.xilinx.spartan import parse_xilinx_spartan
+from parsers.xilinx.spartan_six import parse_xilinx_spartan_six
+from parsers.xilinx.virtex_five import parse_xilinx_virtex_five
+from parsers.xilinx.virtex_four import parse_xilinx_virtex_four
+from parsers.xilinx.virtex_six import parse_xilinx_virtex_six
+from parsers.xilinx.ultrascale_plus import parse_kintex_ultrascale_plus
+from parsers.xilinx.ultrascale import parse_kintex_ultrascale
 
 PARSE_TABLE = {
     'parse_kintex_ultrascale': parse_kintex_ultrascale,
